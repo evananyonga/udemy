53c6b5405d81b8286cc9af317724606b34e65456
evananyonga <evewish@gmail.com>
2021-07-11 02:10:10 -0700

    using the forEach

--- list.js
+++ list.js
@@ -1,16 +1,37 @@
 window.setTimeout(function() {
   // put all of your JS code from the lecture
-  var todos = [];
+  var todos = [""];
   var input = prompt("What would you like to do");
 
   while (input !== "quit") {
     if (input === "list") {
-      console.log(todos);
+      listTodos()
     } else if (input === "new") {
-      var newInput = prompt("Enter a new todo item")
-      todos.push(newInput);
+      addTodos()
+    } else if (input === "delete") {
+      deleteTodo()
     }
     input = prompt("What would you like to do")
   }
   console.log("Okay, I give up!");
+
+  function listTodos() {
+    todos.forEach((item, index) => {
+      console.log(index + ": " + item);
+    });
+    console.log("********");
+  }
+
+  function addTodos() {
+    var newInput = prompt("Enter a new todo item")
+    todos.push(newInput);
+    console.log("Added a todo item");
+  }
+
+  function deleteTodo() {
+    var indexTodelete = prompt("Enter number of todo you want removed from the list")
+    todos.splice(indexTodelete, 1);
+    console.log("Removed a todo Item");
+  }
+
 }, 500);
